82b46fdaf3d05245a025740962e51b95326229ea
Susmit Basu <bosesusmit2006@gmail.com>
2020-08-11 13:38:01 +0800

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import * from "styles.css"
+function Header() {
+  return (
+    <nav>
+      <ul>
+        <li> Item 1 </li>
+        <li> Item 2 </li>
+        <li> Item 3 </li>
+      </ul>
+    </nav>
+  );
+}
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Main() {
+  return (
+    <div>
+      <p>Main Content area</p>
+    </div>
+  );
+}
+export default Main;
